b06f72b7a7f04b7f06a1dfff45dc5e90fef9a898
Mateus Furquim <mfurquimdev@gmail.com>
2023-11-26 18:59:27 -0300

    Fetch tracks and features

--- src/tracks/view.py
+++ src/tracks/view.py
@@ -43,6 +43,45 @@ class TrackFeatures:
                 yield k, v
 
 
+async def saved_tracks_to_df(result):
+    tracks_df = pd.DataFrame(
+        [
+            {"track_id": item["track"]["id"], "track_name": item["track"]["name"]}
+            for item in result.get("items", [])
+        ]
+    )
+    return tracks_df
+
+
+async def request_saved_tracks(sp, iteration: int):
+    LIMIT: int = int(os.getenv("LIMIT", "50"))
+
+    offset = LIMIT * iteration
+    result = sp.current_user_saved_tracks(limit=LIMIT, offset=offset)
+
+    return result
+
+
+async def request_track_features(sp, tracks_df):
+    track_id_list = tracks_df.track_id.to_list()
+    track_features = sp.audio_features(track_id_list)
+    track_features_df = pd.DataFrame(track_features)
+    track_features_df.rename({"id": "track_id"}, axis=1, inplace=True)
+    track_features_df.drop(
+        columns=[
+            "type",
+            "uri",
+            "track_href",
+            "analysis_url",
+        ],
+        inplace=True,
+    )
+    # WARN: this is assuming that the order is always consistent
+    track_features_df["track_name"] = tracks_df["track_name"]
+
+    return track_features_df
+
+
 @router.get("/saved")
 async def saved_tracks():
     access_token = await get_cached_access_token(sp_oauth)
@@ -54,42 +93,27 @@ async def saved_tracks():
     print("Access token available! Trying to get user information...")
     sp = spotipy.Spotify(access_token)
 
-    LIMIT: int = int(os.getenv("LIMIT", "50"))
     i: int = 0
-    results = []
+    features_result = pd.DataFrame()
     while True:
-        offset = LIMIT * i
-        result = sp.current_user_saved_tracks(limit=LIMIT, offset=offset)
-        if result["next"] is None:
+        result = await request_saved_tracks(sp, i)
+        i += 1
+        print(result["next"])
+
+        if result is None:
             break
 
-        print(result["next"])
-        results.extend(result["items"])
-        i += 1
+        tracks_df = await saved_tracks_to_df(result)
+        if tracks_df is not None and not tracks_df.empty:
+            track_features_df = await request_track_features(sp, tracks_df)
 
-    print(f"Found {len(results)} tracks")
-    tracks_df = pd.DataFrame(
-        [{"track_id": item["track"]["id"], "track_name": item["track"]["name"]} for item in results]
-    )
-    track_id_list = tracks_df.track_id.to_list()
-
-    all_features = sp.audio_features(track_id_list)
-    audio_features_df = pd.DataFrame(all_features)
-    audio_features_df.rename({"id": "track_id"}, axis=1, inplace=True)
-    audio_features_df.drop(
-        columns=[
-            "type",
-            "uri",
-            "track_href",
-            "analysis_url",
-        ],
-        inplace=True,
-    )
-    # WARN: this is assuming that the order is always consistent
-    audio_features_df["track_name"] = tracks_df["track_name"]
+            if features_result.empty:
+                features_result = track_features_df
+            else:
+                features_result = pd.concat([features_result, track_features_df], ignore_index=True)
 
     track_features_list = [
-        TrackFeatures.from_series(row) for _, row in audio_features_df.iterrows()
+        TrackFeatures.from_series(row) for _, row in track_features_df.iterrows()
     ]
 
     return track_features_list
